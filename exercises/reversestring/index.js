02c8b9f2d7d64f629d0829d3a78c256d7d33afd3
Billy Downing <wldowning@gmail.com>
2021-01-08 17:54:41 -0700

    reverse string practice

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split("").reduce((reversed, character) => {
+    return character + reversed;
+  }, "");
+}
+
+module.exports = reverse;
+
+// const arr = str.split("");
+//   return arr.reverse().join("");
+
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
